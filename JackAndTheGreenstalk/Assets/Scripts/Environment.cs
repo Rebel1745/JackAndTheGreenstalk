008b3437b24c9aef584b41ca6f6bb287c57b5cfb
Rebel1745 <31040719+Rebel1745@users.noreply.github.com>
2019-02-23 18:09:12 +0000

    Basic UI
    Panels for environment and plant stats

--- JackAndTheGreenstalk/Assets/Scripts/Environment.cs
+++ JackAndTheGreenstalk/Assets/Scripts/Environment.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Environment : MonoBehaviour
 {
@@ -8,6 +9,7 @@ public class Environment : MonoBehaviour
     void Start()
     {
         InitialiseEnvironment();
+        DisplayStats();
     }
 
     public bool IsOutdoors;
@@ -15,38 +17,57 @@ public class Environment : MonoBehaviour
     public float MinTemperature;
     public float MaxTemperature;
     float temperature;
+    public Text TemperatureText;
 
     public float MinHumidity;
     public float MaxHumidity;
     float humidity;
+    public Text HumidityText;
 
     public float MinpH;
     public float MaxpH;
     float pHLevel;
+    public Text pHLevelText;
+
+    public float MinWaterContent;
+    public float MaxWaterContent;
+    float waterContent;
+    public Text WaterContentText;
+
+    public float MinWaterReplacementRate;
+    public float MaxWaterReplacementRate;
+    float waterReplacementRate;
+    public Text WaterReplacementRateText;
 
     public float MinNitrogenContent;
     public float MaxNitrogenContent;
     float nitrogenContent;
+    public Text NitrogenContentText;
 
     public float MinNitrogenReplacementRate;
     public float MaxNitrogenReplacementRate;
     float nitrogenReplacementRate;
+    public Text NitrogenReplacementRateText;
 
-    public float MinPhosphorousContent;
-    public float MaxPhosphorousContent;
-    float phosphorousConstumption;
+    public float MinPhosphorusContent;
+    public float MaxPhosphorusContent;
+    float phosphorusContent;
+    public Text PhosphorusContentText;
 
-    public float MinPhosphorousReplacementRate;
-    public float MaxPhosphorousReplacementRate;
-    float phosphorousReplacementRate;
+    public float MinPhosphorusReplacementRate;
+    public float MaxPhosphorusReplacementRate;
+    float phosphorusReplacementRate;
+    public Text PhosphorusReplacementRateText;
 
     public float MinPotassiumContent;
     public float MaxPotassiumContent;
     float potassiumContent;
+    public Text PotassiumContentText;
 
     public float MinPotassiumReplacementRate;
     public float MaxPotassiumReplacementRate;
     float potassiumReplacementRate;
+    public Text PotassiumReplacementRateText;
 
     // weather?
 
@@ -56,12 +77,37 @@ public class Environment : MonoBehaviour
         humidity = Random.Range(MinHumidity, MaxHumidity);
         pHLevel = Random.Range(MinpH, MaxpH);
 
+        waterContent = Random.Range(MinWaterContent, MaxWaterContent);
         nitrogenContent = Random.Range(MinNitrogenContent, MaxNitrogenContent);
-        phosphorousConstumption = Random.Range(MinPhosphorousContent, MaxPhosphorousContent);
+        phosphorusContent = Random.Range(MinPhosphorusContent, MaxPhosphorusContent);
         potassiumContent = Random.Range(MinPotassiumContent, MaxPotassiumContent);
 
+        waterReplacementRate = Random.Range(MinWaterReplacementRate, MaxWaterReplacementRate);
         nitrogenReplacementRate = Random.Range(MinNitrogenReplacementRate, MaxNitrogenReplacementRate);
-        phosphorousConstumption = Random.Range(MinPhosphorousReplacementRate, MaxPhosphorousReplacementRate);
+        phosphorusReplacementRate = Random.Range(MinPhosphorusReplacementRate, MaxPhosphorusReplacementRate);
         potassiumReplacementRate = Random.Range(MinPotassiumReplacementRate, MaxPotassiumReplacementRate);
+    }
+
+    public void DisplayStats()
+    {
+        TemperatureText.text = RoundToDP(temperature, 1f).ToString();
+        HumidityText.text = RoundToDP(humidity, 0f).ToString();
+        pHLevelText.text = RoundToDP(pHLevel, 1f).ToString();
+        WaterContentText.text = RoundToDP(waterContent, 0f).ToString();
+        WaterReplacementRateText.text = RoundToDP(waterReplacementRate, 2f).ToString();
+        NitrogenContentText.text = RoundToDP(nitrogenContent, 2f).ToString();
+        NitrogenReplacementRateText.text = RoundToDP(nitrogenReplacementRate, 0f).ToString();
+        PhosphorusContentText.text = RoundToDP(phosphorusContent, 2f).ToString();
+        PhosphorusReplacementRateText.text = RoundToDP(phosphorusReplacementRate, 0f).ToString();
+        PotassiumReplacementRateText.text = RoundToDP(potassiumReplacementRate, 0f).ToString();
+        PotassiumContentText.text = RoundToDP(potassiumContent, 2f).ToString();
+    }
+
+    float RoundToDP(float num, float dp)
+    {
+
+        float multiplier = Mathf.Pow(10, dp);
+
+        return Mathf.Round(num * multiplier) / multiplier;
     }
 }
--- JackAndTheGreenstalk/Assets/Scripts/Plant.cs
+++ JackAndTheGreenstalk/Assets/Scripts/Plant.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Plant : MonoBehaviour
 {
@@ -8,76 +9,104 @@ public class Plant : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlantSeed();
+        DisplayStats();
     }
 
     // Stats
     int ageInDays;
+    public Text AgeInDaysText;
 
     public bool IsAutoflower;
 
-    public float MinFloweringTime;
-    public float MaxFloweringTime;
-    float floweringTime;
+    public float MinFloweringDay;
+    public float MaxFloweringDay;
+    float floweringDay;
+    public Text FloweringDayText;
 
     public float MinHeight;
     public float MaxHeight;
     float maxHeight;
     float currentHeight;
+    public Text CurrentHeightText;
 
     public float MinGrowRate;
     public float MaxGrowRate;
     float maxGrowRate;
-    //float currentGrowRate;
+    float currentGrowRate;
+    public Text CurrentGrowRateText;
 
-    public float MinYeild;
-    public float MaxYeild;
-    float maxYeild;
+    public float MinYield;
+    public float MaxYield;
+    float maxYield;
+    public Text MaxYieldText;
 
     // public Color[] PlantColours; //colours of different bits of the plant
 
     public float MinTHC;
     public float MaxTHC;
     float THCContent;
+    public Text THCContentText;
 
     public float MinCBD;
     public float MaxCBD;
     float CBDContent;
+    public Text CBDContentText;
 
     public float MinpH;
     public float MaxpH;
-    float pHLevel;
+    float idealpHLevel;
+    public Text IdealpHText;
+    //float currentpHLevel;
 
     public float MinTemp;
     public float MaxTemp;
     float idealTemp;
+    public Text IdealTempText;
+    //float currentTemp;
 
     public float MinHumidity;
     public float MaxHumidity;
     float idealHumidity;
+    public Text IdealHumidityText;
+    //float currentHumidity;
 
     public float MinLight;
     public float MaxLight;
+    //float currentLight;
 
     public PLANT_STATE PlantState;
+    public Text PlantStateText;
     // hardiness
 
     // Needs
     public float MinWaterConsumption;
     public float MaxWaterConsumption;
     float waterConsumption;
+    public Text WaterConsumptionText;
+    float currentWaterLevel = 100f;
+    public Text WaterLevelText;
 
     public float MinNitrogenConsumption;
     public float MaxNitrogenConsumption;
     float nitrogenConsumption;
+    public Text NitrogenConsumptionText;
+    float currentNitrogenLevel = 100f;
+    public Text NitrogenLevelText;
 
-    public float MinPhosphorousConsumption;
-    public float MaxPhosphorousConsumption;
-    float phosphorousConstumption;
+    public float MinPhosphorusConsumption;
+    public float MaxPhosphorusConsumption;
+    float phosphorusConsumption;
+    public Text PhosphorusConsumptionText;
+    float currentPhosphorusLevel = 100f;
+    public Text PhosphorusLevelText;
 
     public float MinPotassiumConsumption;
     public float MaxPotassiumConsumption;
     float potassiumConsumption;
+    public Text PotassiumConsumptionText;
+    float currentPotassiumLevel = 100f;
+    public Text PotassiumLevelText;
 
     public void PlantSeed()
     {
@@ -85,18 +114,20 @@ public class Plant : MonoBehaviour
 
         maxHeight = Random.Range(MinHeight, MaxHeight);
         maxGrowRate = Random.Range(MinGrowRate, MaxGrowRate);
-        floweringTime = Random.Range(MinFloweringTime, MaxFloweringTime);
-        maxYeild = Random.Range(MinYeild, MaxYeild);
+        floweringDay = Random.Range(MinFloweringDay, MaxFloweringDay);
+        maxYield = Random.Range(MinYield, MaxYield);
         THCContent = Random.Range(MinTHC, MaxTHC);
         CBDContent = Random.Range(MinCBD, MaxCBD);
-        pHLevel = Random.Range(MinpH, MaxpH);
+        idealpHLevel = Random.Range(MinpH, MaxpH);
         idealTemp = Random.Range(MinTemp, MaxTemp);
         idealHumidity = Random.Range(MinHumidity, MaxHumidity);
 
         waterConsumption = Random.Range(MinWaterConsumption, MaxWaterConsumption);
         nitrogenConsumption = Random.Range(MinNitrogenConsumption, MaxNitrogenConsumption);
-        phosphorousConstumption = Random.Range(MinPhosphorousConsumption, MaxPhosphorousConsumption);
+        phosphorusConsumption = Random.Range(MinPhosphorusConsumption, MaxPhosphorusConsumption);
         potassiumConsumption = Random.Range(MinPotassiumConsumption, MaxPotassiumConsumption);
+
+        currentGrowRate = maxGrowRate;
     }
 
     // move the game along and calculate the new state of the plant
@@ -105,5 +136,41 @@ public class Plant : MonoBehaviour
         ageInDays++;
 
         currentHeight += currentGrowRate;
+        currentWaterLevel -= waterConsumption;
+        currentNitrogenLevel -= nitrogenConsumption;
+        currentPhosphorusLevel -= phosphorusConsumption;
+        currentPotassiumLevel -= potassiumConsumption;
+    }
+
+    public void DisplayStats()
+    {
+        AgeInDaysText.text = ageInDays.ToString();
+        FloweringDayText.text = RoundToDP(floweringDay, 0f).ToString();
+        CurrentHeightText.text = RoundToDP(currentHeight, 2f).ToString();
+        CurrentGrowRateText.text = RoundToDP(currentGrowRate, 2f).ToString();
+        MaxYieldText.text = RoundToDP(maxYield, 0f).ToString();
+        THCContentText.text = RoundToDP(THCContent, 1f).ToString();
+        CBDContentText.text = RoundToDP(CBDContent, 1f).ToString();
+        IdealpHText.text = RoundToDP(idealpHLevel, 2f).ToString();
+        IdealTempText.text = RoundToDP(idealTemp, 1f).ToString();
+        IdealHumidityText.text = RoundToDP(idealHumidity, 0f).ToString();
+        PlantStateText.text = PlantState.ToString();
+        WaterLevelText.text = RoundToDP(currentWaterLevel, 0f).ToString();
+        WaterConsumptionText.text = RoundToDP(waterConsumption, 2f).ToString();
+        NitrogenLevelText.text = RoundToDP(currentNitrogenLevel, 0f).ToString();
+        NitrogenConsumptionText.text = RoundToDP(nitrogenConsumption, 2f).ToString();
+        PhosphorusLevelText.text = RoundToDP(currentPhosphorusLevel, 0f).ToString();
+        PhosphorusConsumptionText.text = RoundToDP(phosphorusConsumption, 2f).ToString();
+        PotassiumLevelText.text = RoundToDP(currentPotassiumLevel, 0f).ToString();
+        PotassiumConsumptionText.text = RoundToDP(potassiumConsumption, 2f).ToString();
+
+    }
+
+    float RoundToDP(float num, float dp)
+    {
+
+        float multiplier = Mathf.Pow(10, dp);
+
+        return Mathf.Round(num * multiplier) / multiplier;
     }
 }
